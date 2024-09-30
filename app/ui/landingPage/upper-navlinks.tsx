c8ba727ca2f8e1529a85459894bdd916ab61d3fb
Moshaab <maxassanciiro@gmail.com>
2024-09-30 10:59:14 +0300

    added testmonials and animation

--- app/ui/landingPage/upper-navlinks.tsx
+++ app/ui/landingPage/upper-navlinks.tsx
@@ -6,8 +6,8 @@ import { PhoneIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
 import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 const links = [
-    { name: 'Any Questions? Call Us: 0740 749 907 or 0768 463 768', href: 'tel:0740749907', icon: PhoneIcon },
-    { name: 'Email: info@silveroakrealestate.co.ke', href: 'mailto:info@silveroakrealestate.co.ke', icon: EnvelopeIcon }
+    { name: 'Any Questions? Call Us: 0729111768', href: 'tel:0729111768', icon: PhoneIcon },
+    { name: 'Email: mxassanciiro@gmail.com', href: 'mailto:mxassanciiro@gmail.com', icon: EnvelopeIcon }
 ];
 
 export default function UpperNavlinks() {
@@ -36,16 +36,16 @@ export default function UpperNavlinks() {
 
             <div className="flex space-x-4 mt-4 md:mt-0">
                 {/* Social Media Icons */}
-                <Link href="http://facebook.com/silveroakrealestate" className="text-white hover:text-blue-600" aria-label="Facebook">
+                <Link href="http://facebook.com" className="text-white hover:text-blue-600" aria-label="Facebook">
                     <FaFacebook className="w-5 h-5 md:w-6 md:h-6" />
                 </Link>
                 <Link href="https://x.com" className="text-white hover:text-blue-400" aria-label="X">
                     <FaTwitter className="w-5 h-5 md:w-6 md:h-6" />
                 </Link>
-                <Link href="https://www.instagram.com/silveroakrealestate/" className="text-white hover:text-pink-500" aria-label="Instagram">
+                <Link href="https://www.instagram.com" className="text-white hover:text-pink-500" aria-label="Instagram">
                     <FaInstagram className="w-5 h-5 md:w-6 md:h-6" />
                 </Link>
-                <Link href="https://www.youtube.com/channel/UC-tMhaxlMOvjnjEC9aRMqIw" className="text-white hover:text-red-600" aria-label="YouTube">
+                <Link href="https://www.youtube.com" className="text-white hover:text-red-600" aria-label="YouTube">
                     <FaYoutube className="w-5 h-5 md:w-6 md:h-6" />
                 </Link>
             </div>
