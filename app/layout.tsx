3c7bec5bcd2050a57916a227e4cf5e0e17889f55
Shaab <mxassanciiro@gmail.com>
2024-12-14 19:12:30 +0300

    applied favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import '@/app/ui/global.css';
 
 
 export const metadata: Metadata = {
-  title: "HiddoJacayl",
-  description: "Developed by Shacab Technologies",
+  title: "HiilDhaxal",
+  description: "this is about Somali heritage, culture, history, language and its rich literature. Wuxuu ku saabsanyahay hiddaha, dhaqanka, taariikhda iyo luuqadda Soomaaliyeed. Waxaan meesha ka maqnayn suugaanta qaniga ku tahay Soomaalidu sida heesaha iyo gabayada ama maansooyinka leh murtida.",
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <script src="//code.tidio.co/uxesf4jarnatcc2fltflyi4wtx6taknq.js" async></script>
+      {/* <script src="//code.tidio.co/uxesf4jarnatcc2fltflyi4wtx6taknq.js" async></script> */}
       <body className={inter.className}>{children}</body>
     </html>
   );
