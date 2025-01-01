59dc98d23941b31ac080025cdc379269e27b91aa
Shaab <mxassanciiro@gmail.com>
2025-01-01 17:10:45 +0300

    updated the layout for better seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,46 @@
 import type { Metadata } from "next";
-import { inter, lusitana } from '@/app/ui/fonts';
-import '@/app/ui/global.css';
-import Navbar from "./ui/landingPage/components/Navbar/Navbar";
-import Footer from "./ui/landingPage/components/Footer/Footer";
-import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
+import dynamic from "next/dynamic";
+import { Analytics } from "@vercel/analytics/react";
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
 
-
+const Navbar = dynamic(() => import("./ui/landingPage/components/Navbar/Navbar"), { ssr: true });
+const Footer = dynamic(() => import("./ui/landingPage/components/Footer/Footer"), { ssr: true });
 
 export const metadata: Metadata = {
-  title: "HiilDhaxal",
-  description: "This is about Somali heritage, culture, history, language and its rich literature. Wuxuu ku saabsanyahay hiddaha, dhaqanka, taariikhda iyo luuqadda Soomaaliyeed. Waxaan meesha ka maqnayn suugaanta qaniga ku tahay Soomaalidu sida heesaha iyo gabayada ama maansooyinka leh murtida.",
+  title: "HiilDhaxal | Somali Heritage and Culture",
+  description: "This is about Somali heritage, culture, history, language, and its rich literature, including songs, poetry, and insightful narratives. Wuxuu ku saabsanyahay hiddaha, dhaqanka, taariikhda iyo luuqadda Soomaaliyeed. Waxaan meesha ka maqnayn suugaanta qaniga ku tahay Soomaalidu sida heesaha iyo gabayada ama maansooyinka leh murtida",
+  keywords: "Somali heritage, Somali culture, Somali history, Somali literature, Somali poetry, Somali language",
+  openGraph: {
+    title: "HiilDhaxal",
+    description: "Explore the culture, history, and literature of Somalia.",
+    type: "website",
+    locale: "en_US",
+    url: "https://hiildhaxal.online",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      {/* <script src="//code.tidio.co/uxesf4jarnatcc2fltflyi4wtx6taknq.js" async></script> */}
-
-       {/* TrustBox script  */}
-<script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-
-     
+      <head>
+        <Script
+          src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+          strategy="lazyOnload"
+        />
+        {/* Any additional meta tags */}
+      </head>
       <body className={inter.className}>
         <Navbar />
-        
-        {children}
+        <main>{children}</main>
         <Analytics />
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
