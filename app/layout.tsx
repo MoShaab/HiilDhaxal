cf240836484c8fd751c8584d0641964ab04dd65d
Shaab <mxassanciiro@gmail.com>
2025-01-04 15:43:48 +0300

    added og

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const Footer = dynamic(() => import("./ui/landingPage/components/Footer/Footer")
 export const metadata: Metadata = {
   title: "HiilDhaxal | Somali Heritage and Culture",
   description: "This is about Somali heritage, culture, history, language, and its rich literature, including songs, poetry, and insightful narratives. Wuxuu ku saabsanyahay hiddaha, dhaqanka, taariikhda iyo luuqadda Soomaaliyeed. Waxaan meesha ka maqnayn suugaanta qaniga ku tahay Soomaalidu sida heesaha iyo gabayada ama maansooyinka leh murtida",
-  keywords: "Somali heritage, Somali culture, Somali history, Somali literature, Somali poetry, Somali language",
+  keywords: "Somali heritage, Dhaxal, Somali culture, Dhaqan, Somali history, Taariikh, Somali literature, Somali poetry, Gabayo, Maansooyin, Somali language, Afka Hooyo",
   openGraph: {
     title: "HiilDhaxal",
-    description: "Explore the culture, history, and literature of Somalia.",
+    description: "Hiddiyo dhaqan, Taariikh, Suugaanta, Khayraadka.",
     type: "website",
     locale: "en_US",
     url: "https://hiildhaxal.online",
